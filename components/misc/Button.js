255ac933b84dccd398465dbc50063986936b4196
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-04 20:07:47 +0700

    add type button

--- components/misc/Button.js
+++ components/misc/Button.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 export const BUTTON_TYPE_CLASS = {
-  base: 'py-3 lg:py-4 px-12 lg:px-16 rounded-lg hover:shadow-orange-md transition-all outline-none text-white-500 font-semibold bg-orange-500 ',
-  base_outline: 'font-medium tracking-wide py-2 px-5 sm:px-8 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange ',
-  playstore: 'font-medium tracking-wide py-4 px-8 sm:px-14 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-lg capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange bg-playstore bg-cover bg-no-repeat ',
-  appstore: 'font-medium tracking-wide py-4 px-8 sm:px-14 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-lg capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange bg-appstore bg-cover bg-no-repeat ',
+  base: 'py-3 lg:py-4 px-12 lg:px-16 rounded-lg hover:shadow-primary-md transition-all outline-none text-white-500 font-semibold bg-primary-500 ',
+  base_outline: 'font-medium tracking-wide py-2 px-5 sm:px-8 border border-primary-500 text-primary-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-primary-500 hover:text-white-500 transition-all hover:shadow-primary ',
+  playstore: 'font-medium tracking-wide py-4 px-8 sm:px-14 border border-primary-500 text-primary-500 bg-white-500 outline-none rounded-lg capitalize hover:bg-primary-500 hover:text-white-500 transition-all hover:shadow-primary bg-playstore bg-cover bg-no-repeat ',
+  appstore: 'font-medium tracking-wide py-4 px-8 sm:px-14 border border-primary-500 text-primary-500 bg-white-500 outline-none rounded-lg capitalize hover:bg-primary-500 hover:text-white-500 transition-all hover:shadow-primary bg-appstore bg-cover bg-no-repeat ',
 };
-
-/* const getButtonType = (buttonType = BUTTON_TYPE_CLASS.base) => (
-  {
-    [BUTTON_TYPE_CLASS.base]
-  }
-) */
 
 const Button = ({ children, buttonType = BUTTON_TYPE_CLASS.base, addClass = '' }) => {
   return (

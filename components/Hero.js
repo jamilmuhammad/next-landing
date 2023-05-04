9706142c998f7aeffa92cd71501908a32216bcaf
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-04 20:29:22 +0700

    remove unused code

--- components/Hero.js
+++ components/Hero.js
@@ -6,25 +6,7 @@ import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 import Button, { BUTTON_TYPE_CLASS } from "./misc/Button";
 
-const Hero = ({
-  listUser = [
-    {
-      name: "Users",
-      number: "390",
-      icon: "/assets/Icon/heroicons_sm-user.svg",
-    },
-    {
-      name: "Locations",
-      number: "20",
-      icon: "/assets/Icon/gridicons_location.svg",
-    },
-    {
-      name: "Server",
-      number: "50",
-      icon: "/assets/Icon/bx_bxs-server.svg",
-    },
-  ],
-}) => {
+const Hero = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
 
   return (
@@ -44,59 +26,17 @@ const Hero = ({
               WIGO adalah platform yang dibuat oleh anak bangsa khusus untuk pariwisata, anda bisa mendapatkan referensi tempat wisata, mulai dari wisata halal, desa wisata, eat & view, hidden gems, dan lain-lain.
             </p>
             <motion.div
-              className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8
-              /12 sm:w-auto mx-auto sm:mx-0"
-              key={2}
-              custom={{ duration: 2 + 2 }}
+              className="flex items-center justify-start sm:justify-start py-4 sm:py-6 w-8
+              /12 sm:w-auto sm:mx-0 space-x-6"
+              custom={{ duration: 2 }}
               variants={scrollAnimation}
             >
               <Button buttonType={BUTTON_TYPE_CLASS.playstore} ></Button>
               <Button buttonType={BUTTON_TYPE_CLASS.appstore} ></Button>
             </motion.div>
           </div>
-          {/* <div className="flex w-full">
-            <motion.div className="h-full w-full" variants={scrollAnimation}>
-              <Image
-                src="/assets/Illustration1.png"
-                alt="VPN Illustrasi"
-                quality={100}
-                width={612}
-                height={383}
-                layout="responsive"
-              />
-            </motion.div>
-          </div> */}
         </motion.div>
       </ScrollAnimationWrapper>
-      {/* <div className="relative w-full flex max-w-screen-xl px-8 xl:px-16 mx-auto">
-        <ScrollAnimationWrapper
-          className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
-          {listUser.map((listUsers, index) => (
-            <motion.div
-              className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0"
-              key={index}
-              custom={{ duration: 2 + index }}
-              variants={scrollAnimation}
-            >
-              <div className="flex mx-auto w-40 sm:w-auto">
-                <div className="flex items-center justify-center bg-orange-100 w-12 h-12 mr-6 rounded-full">
-                  <img src={listUsers.icon} className="h-6 w-6" />
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-xl text-black-600 font-bold">
-                    {listUsers.number}+
-                  </p>
-                  <p className="text-lg text-black-500">{listUsers.name}</p>
-                </div>
-              </div>
-            </motion.div>
-          ))}
-        </ScrollAnimationWrapper>
-        <div
-          className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-64 sm:h-48 top-0 mt-8 mx-auto left-0 right-0"
-          style={{ filter: "blur(114px)" }}
-        ></div>
-      </div> */}
     </div>
   );
 };

96bb54f7960fc33674df878dba1a9c147328f77c
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-04 20:30:04 +0700

    add link mailto, update assets

--- components/Feature.js
+++ components/Feature.js
@@ -3,6 +3,7 @@ import React, { useMemo } from "react";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+import Link from "next/link";
 
 const features = [
   "Powerfull online protection.",
@@ -22,8 +23,8 @@ const Feature = () => {
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/feature-payment.png"
-              alt="VPN Illustrasi"
+              src="/assets/wigo_app.png"
+              alt="Wigo App"
               layout="responsive"
               quality={100}
               height={55}
@@ -41,10 +42,10 @@ const Feature = () => {
               Pengunjung dapat membeli tiket melalui platform WIGO
               dan juga dapat membeli merchandise melalui QRIS.
             </p>
-            <ul className="text-black-500 self-start list-inside ml-8">
+            <ul className="text-black-500 self-start list-inside">
               {features.map((feature, index) => (
                 <motion.li
-                  className="relative circle-check custom-list"
+                  className="relative custom-list flex flex-row"
                   custom={{ duration: 2 + index }}
                   variants={scrollAnimation}
                   key={feature}
@@ -54,15 +55,28 @@ const Feature = () => {
                       duration: .2
                     }
                   }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-primary-100">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
                   {feature}
                 </motion.li>
               )
               )}
             </ul>
+            <motion.div className="mr-auto mt-4">
+              <Link href="mailto:hello@wondergo.id">
+                <a className="text-primary-100 capitalize tracking-wide hover:text-orange-500 transition-all flex flex-row items-center">
+                  Find out more
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                  </svg>
+                </a>
+              </Link>
+            </motion.div>
           </motion.div>
         </ScrollAnimationWrapper>
       </div>
-      {/* <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
         <ScrollAnimationWrapper>
 
           <motion.div className="flex flex-col items-start justify-center mr-auto w-full lg:w-9/12" variants={scrollAnimation}>
@@ -70,13 +84,13 @@ const Feature = () => {
               We Provide Analytic for Merchant
             </h3>
             <p className="my-2 text-black-500">
-            Sebagai mitra Anda, Kami menyediakan dashboard analitik
-             untuk pertumbuhan obyek wisata Anda.
+              Sebagai mitra Anda, Kami menyediakan dashboard analitik
+              untuk pertumbuhan obyek wisata Anda.
             </p>
-            <ul className="text-black-500 self-start list-inside ml-8">
+            <ul className="text-black-500 self-start list-inside">
               {features.map((feature, index) => (
                 <motion.li
-                  className="relative circle-check custom-list"
+                  className="relative custom-list flex flex-row"
                   custom={{ duration: 2 + index }}
                   variants={scrollAnimation}
                   key={feature}
@@ -86,19 +100,32 @@ const Feature = () => {
                       duration: .2
                     }
                   }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-primary-100">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
                   {feature}
                 </motion.li>
               )
               )}
             </ul>
+            <motion.div className="mr-auto mt-4">
+              <Link href="mailto:hello@wondergo.id">
+                <a className="text-primary-100 capitalize tracking-wide hover:text-orange-500 transition-all flex flex-row items-center">
+                  Find out more
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                  </svg>
+                </a>
+              </Link>
+            </motion.div>
           </motion.div>
         </ScrollAnimationWrapper>
 
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/feature-analytic.png"
-              alt="VPN Illustrasi"
+              src="/assets/wigo_dashboard.png"
+              alt="Wigo Dashboard"
               layout="responsive"
               quality={100}
               height={55}
@@ -106,7 +133,7 @@ const Feature = () => {
             />
           </motion.div>
         </ScrollAnimationWrapper>
-      </div> */}
+      </div>
     </div>
   );
 };

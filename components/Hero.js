62eade823eed3017dc4cb39dc6c083b0446edeb3
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-06 04:20:52 +0700

    update wording, update assets playstore&appstore

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,11 @@
 import React, { useMemo } from "react";
-import Image from "next/image";
+import Image from "next/future/image";
 import ButtonPrimary from "./misc/ButtonPrimary";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 import Button, { BUTTON_TYPE_CLASS } from "./misc/Button";
+import { Link } from "react-scroll";
 
 const Hero = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -20,19 +21,25 @@ const Hero = () => {
           variants={scrollAnimation}>
           <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
             <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-white-500 leading-normal">
-              One stop recreation only on <strong>WIGO</strong>.
+              <strong>WIGO</strong>
             </h1>
             <p className="text-white-300 mt-8 mb-2">
-              WIGO adalah platform yang dibuat oleh anak bangsa khusus untuk pariwisata, anda bisa mendapatkan referensi tempat wisata, mulai dari wisata halal, desa wisata, eat & view, hidden gems, dan lain-lain.
+              Indonesia's 1st tourism-focused marketplace and social media platform. Connect with
+              partners nationwide, enjoy unforgettable experiences. Prioritizing quality, usability, and
+              safety, we benefit both users and partners. Join WIGO now for the best travel
+              experiences in Indonesia.
             </p>
             <motion.div
-              className="flex items-center justify-start sm:justify-start py-4 sm:py-6 w-8
-              /12 sm:w-auto sm:mx-0 space-x-6"
+              className="flex items-center justify-start sm:w-auto sm:mx-0 space-x-6"
               custom={{ duration: 2 }}
               variants={scrollAnimation}
             >
-              <Button buttonType={BUTTON_TYPE_CLASS.playstore} ></Button>
-              <Button buttonType={BUTTON_TYPE_CLASS.appstore} ></Button>
+              <Link to="/">
+                <Image src="/assets/appstore1.png" width={250} height={150} sizes="(max-width: 768px) 100%, (max-width: 1200px) 50%, 33%" quality={75} alt="Wigo App Store"></Image>
+              </Link>
+              <Link to="/">
+                <Image src="/assets/playstore1.png" width={250} height={150} sizes="(max-width: 768px) 100%, (max-width: 1200px) 50%, 33%" quality={75} alt="Wigo Play Store"></Image>
+              </Link>
             </motion.div>
           </div>
         </motion.div>

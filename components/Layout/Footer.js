1c64eb94a146f382ca97acf370f9d4d5cc4fec85
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-06 04:22:41 +0700

    update wording

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -10,14 +10,17 @@ const Footer = () => {
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <Image
-            src="/assets/logo_wigo.png"
-            width={135}
-            height={85}
-            
+            src="/assets/logo_wigo1.png"
+            width={65}
+            height={65}
+
             alt="Wigo Indonesia"
           />
           <p className="mb-4">
-            <strong className="font-medium">WIGO</strong> Lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            <strong className="font-medium">WIGO</strong> <br/>
+            Indonesia's 1st tourism-focused marketplace and social media platform. Connect with
+            partners nationwide, enjoy unforgettable experiences. Join WIGO now for the best
+            travel experiences in Indonesia.
           </p>
           <div className="flex w-full mt-2 mb-8 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
@@ -30,7 +33,7 @@ const Footer = () => {
               <Instagram className="h-6 w-6" />
             </div>
           </div>
-          <p className="text-gray-400">©{new Date().getFullYear()} - LaslesVPN</p>
+          <p className="text-gray-400">©{new Date().getFullYear()} - PT Jelajah Wisata Digital</p>
         </div>
         <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
           <p className="text-black-600 mb-4 font-medium text-lg">Product</p>

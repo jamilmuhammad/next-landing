22af763166b6918fc0ef478d5545fd68c8cabcd7
Muhammad Jamil <muhammad.jamil@lumoshive.com>
2023-05-06 04:22:19 +0700

    update wording, add icon heroicon

--- components/Profile.js
+++ components/Profile.js
@@ -12,29 +12,22 @@ const Profile = () => {
       className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
       id="profile"
     >
-      <div className="max-w-screen-xl  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
+      <div className="max-w-screen-lg  px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
         <div className="overflow-hidden flex flex-col w-full">
           <ScrollAnimationWrapper className="items-center mx-auto">
-            <motion.div className="grid mt-8">
+            <motion.div className="grid my-6">
               <motion.h3
                 variants={scrollAnimation}
                 className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
               >
-                Lorem ipsum dolor s
+                Connecting The World of <span className="underline underline-offset-[3px] relative text-primary-600">Tourism</span>
               </motion.h3>
-              <motion.p
-                variants={scrollAnimation}
-                className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
-              >
-                Let's choose the package that is best for you and explore it happily
-                and cheerfully.
-              </motion.p>
             </motion.div>
-              <motion.div className="relative overflow-hidden rounded-lg shadow-md dark:shadow-gray-800">
-                <video controls loop >
-                  <source src="/assets/intro_wigo.mp4" type="video/mp4" />
-                </video>
-                {/* <div className="absolute bottom-1/2 top-1/2 left-2.8/4 right-2/4 text-center">
+            <motion.div className="relative overflow-hidden rounded-lg shadow-md dark:shadow-gray-800">
+              <video controls loop >
+                <source src="/assets/intro_wigo.mp4" type="video/mp4" />
+              </video>
+              {/* <div className="absolute bottom-1/2 top-1/2 left-2.8/4 right-2/4 text-center">
                   <a href="#!" data-type="youtube" data-id="yba7hPeTSjk" className="lightbox rounded-full shadow-lg dark:shadow-gray-500 inline-flex items-center justify-center bg-gray-100 dark:bg-gray-600 hover:bg-gray-100 dark:hover:bg-indigo-600 text-indigo-600 hover:text-white duration-500 ease-in-out">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-20 h-20 inline-flex items-center justify-center stroke-primary-500">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -42,7 +35,7 @@ const Profile = () => {
                     </svg>
                   </a>
                 </div> */}
-              </motion.div>
+            </motion.div>
           </ScrollAnimationWrapper>
         </div>
         <div className="flex flex-col w-full my-16" id="testimoni">
@@ -51,11 +44,16 @@ const Profile = () => {
               <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
                 <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
                   <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
-                    Have interested <br /> Get Special Features!
+                    Have Question? <br /> Get in touch!
                   </h5>
-                  <p>Let's be merchant with us.</p>
+                  <p>If you have any questions or inquiries, please do not hesitate to contact us!</p>
                 </div>
-                <Button>Contact Us</Button>
+                <Button addClass="flex flex-row">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                  </svg>
+                  Contact Us
+                </Button>
               </div>
               <div
                 className="absolute bg-black-600 opacity-5 w-11/12 rounded-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"

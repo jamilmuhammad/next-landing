ded31fba4b84b5124a6c4ea18253c358fa27e3ff
jamilmuhammad <mjamil1339@gmail.com>
2023-05-12 20:08:46 +0700

    add validation for prop checked to input readonly

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
       </a>
       <div className="fixed top-[30%] -right-2 z-50">
         <span className="relative inline-block rotate-90">
-          <input type="checkbox" checked={darkMode} className=" checkbox opacity-0 absolute" id="chk" />
+          <input type="checkbox" checked={darkMode} readOnly className=" checkbox opacity-0 absolute" id="chk" />
           <label onClick={changeTheme} className={" label bg-black-500 dark:bg-white-300 shadow dark:shadow-gray-800 cursor-pointer rounded-full flex justify-between items-center p-1 w-14 h-8"}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-6 stroke-yellow-500">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />

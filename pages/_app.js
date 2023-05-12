edd1eb2fad4634e752e7eb66d07a2ce4e61ae126
jamilmuhammad <mjamil1339@gmail.com>
2023-05-12 19:45:30 +0700

    implement global context, add style for dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "../styles/tailwind.css";
 import "../styles/slick.css";
+import { GlobalContextProvider } from "../contexts/GLobalState";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="dark:bg-black-600">
+      <GlobalContextProvider>
+        <Component {...pageProps} />
+      </GlobalContextProvider>
+    </div>
+  );
 }
 
 export default MyApp;
